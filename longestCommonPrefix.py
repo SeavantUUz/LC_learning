b77487b71d9207704107125de4091460259b47f3
AprocySanae <axdiaoqi220@gmail.com>
2015-06-14 19:53:34 +0800

    add longestCOmmonPrefix

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,3 +1,33 @@
 # coding: utf-8
 __author__ = 'AprocySanae'
 __date__ = '15/6/14'
+
+
+def longestCommonPrefix(strs):
+    def is_common_prefix(endpoint, prefix):
+        for str in strs:
+            if str[:endpoint] != prefix:
+                return False
+        else:
+            return True
+    shortest_string = ''
+    shortest_string_length = 65535
+    for str in strs:
+        length = len(str)
+        if length < shortest_string_length:
+            shortest_string = str
+            shortest_string_length = length
+    left = 0
+    right = shortest_string_length - 1
+    while left <= right:
+        mid = (left + right) / 2
+        if is_common_prefix(mid+1, shortest_string[:mid+1]):
+            left = mid + 1
+        else:
+            right = mid - 1
+    mid = (left + right) / 2
+    return shortest_string[:mid+1]
+
+
+if __name__ == '__main__':
+    print longestCommonPrefix(["a","aba","abc"])
--- romanTointeger.py
+++ romanTointeger.py
@@ -1,3 +1,33 @@
 # coding: utf-8
 __author__ = 'AprocySanae'
 __date__ = '15/6/14'
+
+
+def romanToInteger(s):
+    mapping = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    size = len(s)
+    index = size - 1
+    result = 0
+    while index >= 0:
+        current = s[index]
+        current_value = mapping[current]
+        pre = index - 1
+        if pre >= 0:
+            pre_char = s[pre]
+            pre_char_value = mapping[pre_char]
+            if pre_char_value < current_value:
+                result += current_value - pre_char_value
+                index = pre
+            else:
+                result += current_value
+        else:
+            result += current_value
+        index -= 1
+    return result
+
+
+if __name__ == '__main__':
+    print romanToInteger('XLV')
+
+
+
