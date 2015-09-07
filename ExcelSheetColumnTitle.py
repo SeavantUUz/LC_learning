5be312a87201f4882da5f704c5b77b9373dd6f78
AprocySanae <axdiaoqi220@gmail.com>
2015-09-07 23:27:59 +0800

    excelSheetColumnTitle

---
+++ ExcelSheetColumnTitle.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+__author__ = 'AprocySanae'
+__date__ = '15/9/7'
+
+
+def convertToTitle(n):
+    result = []
+    while n != 0:
+        r = (n-1) % 26 + 1
+        result.insert(0, chr(r+64))
+        n = (n-1) / 26
+    return ''.join(result)
+
+
+if __name__ == '__main__':
+    print convertToTitle(3333)
