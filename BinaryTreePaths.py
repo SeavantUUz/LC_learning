e56acf83fd1ad823f0a85bc5db36f12ef915e23c
AprocySanae <axdiaoqi220@gmail.com>
2015-10-19 22:59:13 +0800

    BinaryTreePaths

---
+++ BinaryTreePaths.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+__author__ = 'AprocySanae'
+__date__ = '15/10/19'
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        self.result = []
+        if not root:
+            return []
+        else:
+            self.helper(root, '')
+        return self.result
+
+
+    def helper(self, root, one_path):
+        if not root:
+            return
+        if not one_path:
+            one_path = str(root.val)
+        else:
+            one_path = one_path + '->{}'.format(root.val)
+        if not root.left and not root.right:
+            self.result.append(one_path)
+        else:
+            self.helper(root.left, one_path)
+            self.helper(root.right, one_path)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.right = TreeNode(5)
+    print s.binaryTreePaths(root)
